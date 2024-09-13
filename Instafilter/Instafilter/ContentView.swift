4008aeb72fdd00754dc705230285d73eacca17c1
galih <samudragalih94@gmail.com>
2024-09-13 11:22:49 +0700

    example(confirmationDialog): example using confirmationDialog

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+    
     var body: some View {
-        Text("Hello world")
+        Button("Hello world") {
+            showingConfirmation = true
+        }
+        .frame(width: 300, height: 300)
+        .foregroundStyle(.black)
+        .background(backgroundColor)
+        .confirmationDialog("Change Background", isPresented: $showingConfirmation) {
+            Button("Red") { backgroundColor = .red }
+            Button("Green") { backgroundColor = .green }
+            Button("Blue") { backgroundColor = .blue }
+            Button("Cancel", role: .cancel) {  }
+        } message: {
+            Text("Select new color")
+        }
     }
 }
 
