2ca96cb0bdcb796d3e0f03e488b58daa7c4b084f
galih <samudragalih94@gmail.com>
2024-09-17 17:13:29 +0700

    clean code

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,52 +5,11 @@
 //  Created by Galih Samudra on 13/09/24.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-        }
-        .onAppear(perform: loadImage)
-    }
-    
-    func loadImage() {
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-        let context = CIContext()
-        let currentFilter = CIFilter.twirlDistortion()
-        currentFilter.inputImage = beginImage
-        
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-        }
-        if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
-        }
-        if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
-        }
-        
-        /// get image CIImage from currentFilter
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        /// get CGImage from CIImage
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-        
-        /// convert CGImage to UIImage
-        let uiImage = UIImage(cgImage: cgImage)
-        
-        /// convert UIImage to Image
-        image = Image(uiImage: uiImage)
+        Text("Hello world")
     }
 }
 
