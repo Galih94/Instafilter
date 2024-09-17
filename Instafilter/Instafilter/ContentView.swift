77d7ce5ca47ff8e53e53db5876f69c51edc9fde9
galih <samudragalih94@gmail.com>
2024-09-17 17:20:37 +0700

    example(ContentUnavailableView): example implementation `ContentUnavailableView` with label, desciption and action

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ContentUnavailableView("No snippets", systemImage: "swift", description: Text("You don't have any saved snippets yet."))
+        ContentUnavailableView(label: {
+            Label("No Snippets", systemImage: "swift")
+        }, description: {
+        Text("You don't have any saved snippets yet.")
+        }, actions: {
+            Button("Create Snippet") {
+                /// create snippet
+            }
+            .buttonStyle(.borderedProminent)
+        })
     }
 }
 
