96f0a4dfc3fa583d919700ec43b729f5ba79123d
galih <samudragalih94@gmail.com>
2024-09-18 11:21:42 +0700

    clean code

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,11 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    let example = Image(.example)
     var body: some View {
-        ShareLink(item: example, preview: SharePreview("Land Preview", image: example)) {
-            Label("Land Image", systemImage: "magnifyingglass.circle.fill")
-        }
+        Text("Hello world")
     }
 }
 
