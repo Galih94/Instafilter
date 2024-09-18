5dd27515df4431f10117be8f0e942bc57fcf93c8
galih <samudragalih94@gmail.com>
2024-09-18 10:15:38 +0700

    clean code

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -9,16 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ContentUnavailableView(label: {
-            Label("No Snippets", systemImage: "swift")
-        }, description: {
-        Text("You don't have any saved snippets yet.")
-        }, actions: {
-            Button("Create Snippet") {
-                /// create snippet
-            }
-            .buttonStyle(.borderedProminent)
-        })
+        Text("Hello world")
     }
 }
 
