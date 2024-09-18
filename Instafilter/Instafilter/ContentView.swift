b3a558fa167a1186fdde86aef6607efa37e2e076
galih <samudragalih94@gmail.com>
2024-09-18 10:59:46 +0700

    clean code

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,37 +5,11 @@
 //  Created by Galih Samudra on 13/09/24.
 //
 
-import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var pickerItems = [PhotosPickerItem]()
-    @State private var selectedImages = [Image]()
-    
     var body: some View {
-        VStack {
-            /// can use .image(), .any(), .all() and .not() as matching image
-            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)]) ) {
-                Label("Select a picture", systemImage: "photo")
-            }
-            ScrollView {
-                ForEach(0..<selectedImages.count, id: \.self) { i in
-                    selectedImages[i]
-                        .resizable()
-                        .scaledToFit()
-                }
-            }
-        }
-        .onChange(of: pickerItems) {
-            Task {
-                selectedImages.removeAll()
-                for item in pickerItems {
-                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
-                        selectedImages.append(loadedImage)
-                    }
-                }
-            }
-        }
+        Text("Hello world")
     }
 }
 
