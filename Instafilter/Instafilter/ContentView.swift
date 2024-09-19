afca11d2952ef723f34af54a747a22309a293632
galih <samudragalih94@gmail.com>
2024-09-19 11:28:56 +0700

    feat(UI): set basic UI

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        Text("Hello world")
+        NavigationStack {
+            VStack {
+                Spacer()
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a Photo"))
+                }
+                Spacer()
+                HStack(content: {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity, in: 0...1)
+                })
+                .padding(.vertical)
+                HStack(content: {
+                    Button("Change Filter") {
+                        changeFilter()
+                    }
+                    Spacer()
+                })
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+        }
+    }
+    
+    func changeFilter() {
+        
     }
 }
 
