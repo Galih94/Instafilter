380ca87a77e8522e0e4e3fa903eb08d15d71979f
galih <samudragalih94@gmail.com>
2024-09-19 14:40:05 +0700

    feat(Import Image): import image from `PhotosPicker`

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,9 +5,11 @@
 //  Created by Galih Samudra on 13/09/24.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedItem: PhotosPickerItem?
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     
@@ -15,13 +17,17 @@ struct ContentView: View {
         NavigationStack {
             VStack {
                 Spacer()
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a Photo"))
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a Photo"))
+                    }
                 }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
                 Spacer()
                 HStack(content: {
                     Text("Intensity")
@@ -42,6 +48,13 @@ struct ContentView: View {
     
     func changeFilter() {
         
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+        }
     }
 }
 
