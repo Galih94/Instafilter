d171ba07c9cc2a2496c564e2af071d51f39bed7b
galih <samudragalih94@gmail.com>
2024-09-19 10:39:59 +0700

    clean code

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,15 +5,11 @@
 //  Created by Galih Samudra on 13/09/24.
 //
 
-import StoreKit
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.requestReview) var requestReview
     var body: some View {
-        Button("leave a review") {
-            requestReview()
-        }
+        Text("Hello world")
     }
 }
 
